cb077a942ed77820dfedd9bad4943ca0597373f8
Emily Venezia <emilygamergirl99@gmail.com>
2024-07-13 20:20:25 -0400

    prepare for puzzle generation

--- puzzles/puzzle_grid.gd
+++ puzzles/puzzle_grid.gd
@@ -8,3 +8,24 @@ var difficulty: Difficulty
 var solutions: Array[int] = []
 var givens: Array[bool] = []
 
+func _init():
+	solutions.resize(81)
+	givens.resize(81)
+
+func _to_string():
+	var s: String = "---PUZZLEGRID %s---\n" % Difficulty.find_key(difficulty)
+	for row in 9:
+		for col in 9:
+			s += str(solutions[row*9+col])
+			if col % 3 == 2:
+				s += " "
+		s += "    "
+		for col in 9:
+			s += str(solutions[row*9+col]) if givens[row*9+col] else "_"
+			if col % 3 == 2:
+				s += " "
+		s += "\n"
+		if row % 3 == 2:
+			s += "\n"
+	s += "--- ---\n"
+	return s
