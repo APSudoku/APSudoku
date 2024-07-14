cd9cfe588f3a8d7556ac04a7fcd91b08fb2758d5
Emily Venezia <emilygamergirl99@gmail.com>
2024-07-14 08:23:01 -0400

    better mouse selection

--- ui/actual_grid.gd
+++ ui/actual_grid.gd
@@ -1,19 +1,27 @@
 extends GridContainer
 var has_mouse := false
+var clicked_grid := false
 var allow_grid_sel := true
 
+func _process(_delta):
+	if Input.is_action_just_released("mouse_lr"):
+		clicked_grid = false
+		if not has_mouse:
+			allow_grid_sel = false
 func _notification(what):
 	match what:
 		NOTIFICATION_MOUSE_ENTER:
 			has_mouse = true
-			if Input.get_mouse_button_mask() & (MOUSE_BUTTON_MASK_LEFT|MOUSE_BUTTON_MASK_RIGHT):
-				allow_grid_sel = false
-			else: allow_grid_sel = true
+			allow_grid_sel = clicked_grid or not Input.is_action_pressed("mouse_lr")
 		NOTIFICATION_MOUSE_EXIT:
 			has_mouse = false
-			allow_grid_sel = false
+			if not Input.is_action_pressed("mouse_lr"):
+				allow_grid_sel = false
+				clicked_grid = false
+			elif allow_grid_sel: clicked_grid = true
 
 func _gui_input(event):
 	if event is InputEventMouseButton:
-		if not event.pressed and Input.get_mouse_button_mask() == 0:
+		if event.pressed and (event.button_index == MOUSE_BUTTON_LEFT or event.button_index == MOUSE_BUTTON_RIGHT):
+			clicked_grid = true
 			allow_grid_sel = true
