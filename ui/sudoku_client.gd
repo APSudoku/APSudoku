d3e94a0fdf93d96dd3753d29a53864780b21419d
Emily Venezia <emilygamergirl99@gmail.com>
2025-05-11 19:57:16 -0400

    add version to titlebar of window

--- ui/sudoku_client.gd
+++ ui/sudoku_client.gd
@@ -24,6 +24,7 @@ var _entry_mode: SudokuGrid.EntryMode = SudokuGrid.EntryMode.ANSWER
 func _ready():
 	super()
 	get_window().min_size = Vector2(1050,500)
+	get_window().title += " (%s)" % ProjectSettings.get_setting("application/config/version")
 	if Engine.is_editor_hint():
 		await get_tree().create_timer(1).timeout
 		var q := 0
